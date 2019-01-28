79c4e8ba765703030d6b39536bebb96de9fbdc39
Shijia <zhansj39@gmail.com>
2019-01-29 00:23:01 +0800

    Not yet

--- mainUI.py
+++ mainUI.py
@@ -9,35 +9,14 @@ import keyboard
 import testIO
 import testUI
 
-test = testIO.SerialPort(None)
+serial = testIO.SerialPort(None)
 
+serial.scan()
 
-# text mode interface
-print "-------------------------------------"
-test.scan()
-print "-------------------------------------"
-test.list()
-print ""
-test.input()
-print "-------------------------------------"
+root = Tk()
 
-# serial port settings
-test.config(115200, 0.1)
+window = testUI.UI(root)
 
-# show serial port settings
-if test.port.is_open == True:
-	print "Serial port =",test.name
-	print ("Baud rate = 115200")
-	print ("Time out = 0.1")
+window.device['values'] = serial.device
 
-print "-------------------------------------"
-print ("!!! ---Start test--- !!!")
-
-while True:
-	test.send()
-	#print(test.read())
-	time.sleep(0.1)
-	if keyboard.is_pressed('q'):
-		print ("!!! ---Quit test--- !!!")
-		print "-------------------------------------"
-		break
+root.mainloop()
